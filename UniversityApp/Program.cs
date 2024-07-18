feec9a2ebbb4a201435d49bf9011d057624cb939
Milos <gunjicm.milos99@gmail.com>
2024-07-18 17:44:16 +0200

    test

--- UniversityApp/Program.cs
+++ UniversityApp/Program.cs
@@ -1,3 +1,12 @@
+using System.Text;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using UniversityManagament.Data;
+using UniversityManagament.Models;
+using UniversityManagament.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +15,62 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string connectionString = "Server=127.0.0.1,1433;Database=UniversityDB;User Id=sa;Password=Nemamsifru.99;TrustServerCertificate=True;";
+//string connectionString = "Server=localhost; Database=UniversityManagament; Trusted_Connection=True;";
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
+
+var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
+builder.Services.AddSingleton(jwtSettings);
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings.Issuer,
+            ValidAudience = jwtSettings.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey))
+        };
+    });
+
+
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminPolicy", policy => policy.RequireRole("Admin"));
+    options.AddPolicy("UserPolicy", policy => policy.RequireRole("User"));
+    options.AddPolicy("AssistantPolicy", policy => policy.RequireRole("Assistant"));
+    options.AddPolicy("ProfessorPolicy", policy => policy.RequireRole("Professor"));
+});
+
+builder.Services.AddScoped<DepartmentService>();
+builder.Services.AddScoped<ExamPeriodService>();
+builder.Services.AddScoped<ExamService>();
+builder.Services.AddScoped<FacultyService>();
+builder.Services.AddScoped<SubjectService>();
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<FinanceService>();
+builder.Services.AddScoped<TokenService>();
+
 
 var app = builder.Build();
 
